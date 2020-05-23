42794e2569198b5a5ce2b57b38a87aedfb68396e
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-05-23 06:32:19 -0500

    Se agregan archivos de validación Cliente Video 16

--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -2,14 +2,14 @@
     <label for="nombre" class="col-lg-3 control-label requerido">Nombre</label>
 
     <div class="col-lg-8">
-      <input type="text" class="form-control" name="nombre" id="nombre" value="{{old('nombre')}}">
+      <input type="text" class="form-control" name="nombre" id="nombre" value="{{old('nombre')}}" required>
     </div>
 </div>
 <div class="form-group">
     <label for="url" class="col-lg-3 control-label requerido">Url</label>
 
     <div class="col-lg-8">
-      <input type="text" class="form-control" name="url" id="url" value="{{old('url')}}">
+      <input type="text" class="form-control" name="url" id="url" value="{{old('url')}}" required>
     </div>
 </div>
 <div class="form-group">
@@ -17,5 +17,9 @@
 
     <div class="col-lg-8">
       <input type="text" class="form-control" name="icono" id="icono" value="{{old('icono')}}">
+    </div>
+
+    <div class="col-lg-1">
+      <span id="mostrar-icono" class="fa fa-fw {{old("icono")}}"></span>
     </div>
 </div>
