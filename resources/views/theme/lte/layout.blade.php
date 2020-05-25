87fcc20175e4de230c6f3b36c6655e4b93d70b02
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-05-25 03:24:08 -0500

    Videotutorial 17 - Listado Sistema de Menú

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -18,9 +18,8 @@
         folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{asset("assets/$theme/dist/css/skins/_all-skins.min.css")}}">
 
-    <link rel="stylesheet" href="{{asset("assets/css/custom.css")}}">
-
     @yield('styles')
+    <link rel="stylesheet" href="{{asset("assets/css/custom.css")}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -62,10 +61,10 @@
         <!-- AdminLTE App -->
         <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
         <!-- jQuery Validation -->
+        @yield('scriptsPlugins')
         <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
         <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
         <script src="{{asset("assets/js/funciones.js")}}"></script>
-
         @yield('scripts')
     </body>
 </html>
