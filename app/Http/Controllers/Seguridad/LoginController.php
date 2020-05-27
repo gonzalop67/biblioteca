92e61acac61834ac82cdc237d48ce4a0d8c7e527
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-05-27 14:51:17 -0500

    Curso Laravel 5.8 - Parte 20 - Autenticación I

---
+++ app/Http/Controllers/Seguridad/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Seguridad;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+    protected $redirectTo = '/admin';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function index()
+    {
+        return view('seguridad.index');
+    }
+
+    public function username()
+    {
+        return 'usuario';
+    }
+}
---
+++ resources/views/admin/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+    Administrador
+@endsection
+
+@section('contenido')
+    <div class="row">
+        <div class="col-log-12">
+            Bienvenidos
+        </div>
+    </div>
+@endsection
