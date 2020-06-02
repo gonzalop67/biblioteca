940098c895283015cbb0f95ea4ea9b9788166b9e
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-06-02 15:37:57 -0500

    Video 27 - Menú Dinámico Parte Frontal - Biblioteca

--- app/Http/Controllers/Seguridad/LoginController.php
+++ app/Http/Controllers/Seguridad/LoginController.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/';
 
     public function __construct()
     {
@@ -19,6 +19,18 @@ class LoginController extends Controller
     public function index()
     {
         return view('seguridad.index');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $roles = $user->roles()->where('estado', 1)->get();
+        if ($roles->isNotEmpty()) {
+            $user->setSession($roles->toArray());
+        } else {
+            $this->guard()->logout();
+            $request->session()->invalidate();
+            return redirect('seguridad/login')->withErrors(['error' => 'Este usuario no tiene un rol activo']);
+        }
     }
 
     public function username()
--- app/Models/Seguridad/Usuario.php
+++ app/Models/Seguridad/Usuario.php
@@ -3,6 +3,8 @@
 namespace App\Models\Seguridad;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Admin\Rol;
+use Illuminate\Support\Facades\Session;
 
 class Usuario extends Authenticatable
 {
@@ -10,4 +12,24 @@ class Usuario extends Authenticatable
     protected $table = "usuario";
     protected $fillable = ['usuario', 'nombre', 'password'];
     protected $guarded = ['id'];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Rol::class, 'usuario_rol');
+    }
+
+    public function setSession($roles)
+    {
+        if (count($roles) == 1) {
+            Session::put(
+                [
+                    'rol_id' => $roles[0]['id'],
+                    'rol_nombre' => $roles[0]['nombre'],
+                    'usuario' => $this->usuario,
+                    'usuario_id' => $this->id,
+                    'nombre_usuario' => $this->nombre
+                ]
+            );
+        }
+    }
 }
