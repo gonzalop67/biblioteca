e08ad6cd6d5250bba4950d0c306c27df1b4124b0
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-06-12 07:02:15 -0500

    Curso Laravel 6.0 - Parte 42 - Subir Archivos Y Tratamientos de Imagenes I

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -26,7 +26,8 @@ class LibroController extends Controller
      */
     public function crear()
     {
-        //
+        can('crear-libros');
+        return view('libro.crear');
     }
 
     /**
@@ -37,7 +38,8 @@ class LibroController extends Controller
      */
     public function guardar(Request $request)
     {
-        //
+        if ($foto = Libro::setCaratula($request->foto_up))
+            $request->request->add(['foto' => $foto]);
     }
 
     /**
