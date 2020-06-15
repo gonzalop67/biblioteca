6ca498fc196df0f7eb38cea3515d7e21886ecc26
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-06-14 21:30:43 -0500

    Libro Prestamo Completo - WithCount y Filter

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -12,6 +12,11 @@ class Libro extends Model
     protected $table = "libro";
     protected $fillable = ['titulo', 'isbn', 'autor', 'cantidad', 'editorial', 'foto'];
 
+    public function prestamo()
+    {
+        return $this->HasMany(LibroPrestamo::class);
+    }
+
     public static function setCaratula($foto, $actual = false){
         if ($foto) {
             if ($actual) {
